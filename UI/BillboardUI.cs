9531673a3a509704607cc7148f98d58afedb1e84
gesua <40046420+gesua@users.noreply.github.com>
2026-04-17 10:47:44 +0900

    HQ 추가

---
+++ UI/BillboardUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// 월드 UI의 방향을 카메라와 맞춰줌
+/// </summary>
+public class BillboardUI : MonoBehaviour
+{
+    Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void LateUpdate() // 마지막으로 UI가 움직이게
+    {
+        // 카메라의 정면 방향과 자신 게임오브젝트의 정면 방향을 일치시킨다
+        transform.forward = _camera.transform.forward;
+    }
+}
