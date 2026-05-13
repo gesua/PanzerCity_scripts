a9d166e59ebae6242f20096f6dec08207d9f3791
gesua <40046420+gesua@users.noreply.github.com>
2026-05-13 09:14:47 +0900

    인벤토리 열리고 닫힐 때 이미지 변경

--- UI/InventoryUI.cs
+++ UI/InventoryUI.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 인벤토리 UI
 /// </summary>
 public class InventoryUI : MonoBehaviour
 {
+    [SerializeField] Sprite _open;  // 열린거
+    [SerializeField] Sprite _close; // 닫힌거
+    [SerializeField] Image _btnImg; // 가방 버튼 이미지
+
     bool _isActive;
 
     /// <summary>
@@ -14,5 +19,9 @@ public class InventoryUI : MonoBehaviour
     {
         _isActive = !_isActive;
         gameObject.SetActive(_isActive);
+
+        // 가방 이미지 변경
+        if (_isActive) _btnImg.sprite = _open;
+        else _btnImg.sprite = _close;
     }
 }
