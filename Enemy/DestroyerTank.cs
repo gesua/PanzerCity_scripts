07a56fb70546667510b7925cbbf272dc8a8fa9e7
gesua <gesua@hanmail.net>
2026-06-09 17:24:54 +0900

    Revert "구축전차 테스트 중"
    This reverts commit a00e9fa20970b5083f9036a78675567c1884e9c3.

--- Enemy/DestroyerTank.cs
+++ Enemy/DestroyerTank.cs
@@ -6,15 +6,4 @@ using UnityEngine;
 /// </summary>
 public class DestroyerTank : EnemyTank
 {
-    public override void AimAtTarget()
-    {
-        if (Target == null) return;
-
-        Vector3 dir = Target.position - transform.position;
-        dir.y = 0f;
-        if (dir.sqrMagnitude < Mathf.Epsilon) return;
-
-        Quaternion targetRotation = Quaternion.LookRotation(dir);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _model.RotSpeed * Time.deltaTime);
-    }
 }
