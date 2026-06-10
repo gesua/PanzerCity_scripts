010ac404c2ca577fdd2cd1efbacfbe1c5a74bdd9
gesua <40046420+gesua@users.noreply.github.com>
2026-06-10 10:44:42 +0900

    드롭 아이템 크기 일정하게 바꿈

--- Item/DroppedItem.cs
+++ Item/DroppedItem.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 /// <summary>
 /// 바닥에 떨어진 아이템
@@ -8,6 +10,8 @@ public class DroppedItem : MonoBehaviour
 {
     [SerializeField] SpriteRenderer _icon;
     [SerializeField] SpriteRenderer _minimapIcon;
+    
+    float _iconSize = 1.5f; // 고정 크기
     ItemConfig _itemConfig;
 
     // 미니맵에서 아이콘 깜빡임
@@ -21,6 +25,15 @@ public class DroppedItem : MonoBehaviour
         _itemConfig = itemConfig;
         _icon.sprite = itemConfig.IconSprite;
         _minimapIcon.sprite = itemConfig.IconSprite;
+
+        // 크기 조절
+        Vector2 spriteSize = _icon.sprite.bounds.size;
+        float maxSide = Mathf.Max(spriteSize.x, spriteSize.y);
+        float scale = _iconSize / maxSide;
+        _icon.transform.localScale = new Vector3(scale, scale, 1f);
+
+        scale *= 2f; // 미니맵에선 크기 3배
+        _minimapIcon.transform.localScale = new Vector3(scale, scale, 1f);
     }
     void OnEnable()
     {
