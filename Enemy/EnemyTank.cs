2b6c3a91456b8daa6a5c69e58bddf6f9ad3e4cc0
gesua <40046420+gesua@users.noreply.github.com>
2026-07-16 11:28:22 +0900

    플레이어도 바리언트로 바꿈

--- Enemy/EnemyTank.cs
+++ Enemy/EnemyTank.cs
@@ -48,8 +48,8 @@ public class EnemyTank : TankBase, IPoolReturnHandler
     [SerializeField] float _roamSpan = 3f; // 최대 배회 간격
     [SerializeField] float _deadDuration = 5f; // 사망 상태 지속 시간
     [Header("----- 감지 관련 -----")]
-    float _detectionRange = 1000f;                  // 감지 거리(걍 최대치로 할거임)
-    [SerializeField] float _detectionAngle = 30f;   // 감지 각도(부채꼴 반각)
+    float _detectionRange = 1000f; // 감지 거리(걍 최대치로 할거임)
+    [SerializeField] float _detectionAngle = 30f; // 감지 각도(부채꼴 반각)
     [SerializeField] LayerMask _playerLayer = 1 << 8; // 감지할 레이어(플레이어)
     [SerializeField] LayerMask _visionObstacleLayer = 1 << 6; // 시야 차단 레이어(맵)
     [SerializeField] Transform _turret; // 포탑
